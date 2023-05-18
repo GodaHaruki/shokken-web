b8293275a2e1cd6bd7bd0ef8d2f7586777c154f1
Okay-Love <haru.11.saki.11@gmail.com>
2023-05-18 22:36:49 +0900

    change system, impl UserUtil(uncompleted)

--- src/contexts/UserDataCtx.tsx
+++ src/contexts/UserDataCtx.tsx
@@ -1,29 +1,66 @@
 import { useRef, useContext, createContext, useEffect } from "react";
-import { cache } from "./cache";
+import { cache } from "./db";
 
-type User {
+type User = {
   isLogin: boolean
-  userName: string?
+  userName: string | undefined
 }
 
-const UserDataCtx = createContext<React.MutableRefObject<User>>(useRef<User>({
-  isLogin: false,
-  userName: null,
-}))
+type UserUtil = {
+  setIsLogin: (arg0: boolean) => void
+  getIsLogin: () => boolean
 
-export function useUserDataContext(){
+  setUserName: (arg0: string) => void
+  getUserName: () => string | undefined
+}
+
+export function useUserDataContext() {
   return useContext(UserDataCtx);
 }
 
 export function UserDataProvider({ children }: any) {
-  const data = useRef({ isLogin: false, userName: null })
-  useEffect(() => {
-    // todo!
-  }, [data.current])
+  const data = useRef<User>({ isLogin: false, userName: undefined }) // todo! read from cache db
+
+  function setIsLogin(b: boolean) { // todo! add to cache db
+    data.current.isLogin = b
+  }
+
+  function getIsLogin() {
+    return data.current.isLogin
+  }
+
+  function setUserName(name: string) { // todo! add to cache db
+    data.current.userName = name
+  }
+
+  function getUserName() {
+    return data.current.userName
+  }
 
   return (
-    <UserDataCtx.Provider value={data}> // todo!
+    <UserDataCtx.Provider value={{
+      setIsLogin: setIsLogin,
+      getIsLogin: getIsLogin,
+      setUserName: setUserName,
+      getUserName: getUserName,
+    }}>
       {children}
     </UserDataCtx.Provider>
   )
 }
+
+const UserDataCtx = createContext<UserUtil>({
+  setIsLogin: function (_arg0: boolean): void {
+    throw new Error("Function not implemented.");
+  },
+  getIsLogin: function (): boolean {
+    throw new Error("Function not implemented.");
+  },
+  setUserName: function (_arg0: string): void {
+    throw new Error("Function not implemented.");
+  },
+  getUserName: function (): string {
+    throw new Error("Function not implemented.");
+  }
+}
+)
