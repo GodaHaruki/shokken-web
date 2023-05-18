5ddd9a09cd2e80e05e25033ccbeb173ad29a02c0
Okay-Love <haru.11.saki.11@gmail.com>
2023-05-18 21:31:04 +0900

    add tailwindcss linter, add login view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import { Login } from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Login />
     </>
   )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,31 @@
-export function LoginForm() {
+export function Login() {
   return (
     <>
+      <div className="flex h-screen w-screen items-center justify-center bg-white text-sm">
+        <div>
+          <div className="text-center text-xl">
+            Sign in to shokken
+          </div>
+
+          <form className="m-3 h-auto rounded-md border border-gray-300 p-3">
+            <div className="my-2">
+              <label htmlFor="email" className="mb-2 block">email address</label>
+              <input id="email" type="email" className="input-bordered input input-sm w-full max-w-xs" />
+            </div>
+
+            <div className="my-3">
+              <div className="relative mb-2 w-full">
+                <label htmlFor="password" className="mb-2">password</label>
+                <a href="" className="float-right">forgot a password?</a>
+              </div>
+              <input id="password" type="password" autoComplete="current-password" className="input-bordered input input-sm w-full max-w-xs" />
+
+            </div>
+
+            <button className="btn-primary btn-sm btn mt-2 w-full text-xs">login</button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
