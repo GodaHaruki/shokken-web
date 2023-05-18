f0064f99f7a08976530589598c34b73323d02fb6
Okay-Love <haru.11.saki.11@gmail.com>
2023-05-18 22:23:34 +0900

    add login system (not yet completed)

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,10 @@
+import { useRef } from "react"
+import { useUserDataContext } from "../contexts/UserDataCtx"
+
 export function Login() {
+  const email = useRef<string>("")
+  const password = useRef<string>("")
+
   return (
     <>
       <div className="flex h-screen w-screen items-center justify-center bg-white text-sm">
@@ -10,7 +16,7 @@ export function Login() {
           <form className="m-3 h-auto rounded-md border border-gray-300 p-3">
             <div className="my-2">
               <label htmlFor="email" className="mb-2 block">email address</label>
-              <input id="email" type="email" className="input-bordered input input-sm w-full max-w-xs" />
+              <input id="email" type="email" onChange={(e) => email.current = e.target.value} className="input-bordered input input-sm w-full max-w-xs" />
             </div>
 
             <div className="my-3">
@@ -18,14 +24,29 @@ export function Login() {
                 <label htmlFor="password" className="mb-2">password</label>
                 <a href="" className="float-right">forgot a password?</a>
               </div>
-              <input id="password" type="password" autoComplete="current-password" className="input-bordered input input-sm w-full max-w-xs" />
+              <input id="password" type="password" autoComplete="current-password" className="input-bordered input input-sm w-full max-w-xs" onChange={(e) => password.current = e.target.value} />
 
             </div>
 
-            <button className="btn-primary btn-sm btn mt-2 w-full text-xs">login</button>
+            <button onClick={(_e) => handleOnClick(email.current, password.current)} className="btn-primary btn-sm btn mt-2 w-full text-xs">login</button>
           </form>
         </div>
       </div>
     </>
   )
+}
+
+function handleOnClick(email: string, password: string) {
+  // todo!
+  console.log("email: ", email)
+  console.log("password: ", password)
+
+  // const { setIsLogin, setUserName } = useUserDataContext()
+  // fetch 
+  // if success
+  // setIsLogin(true)
+  // else
+
+  // fetch
+  // setUserName(name)
 }
