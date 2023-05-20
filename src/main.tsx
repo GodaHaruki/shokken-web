be4b56896ec0b8365b218036db1749b3a624a97c
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-05-20 06:02:36 +0000

    add UserDataProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App.tsx'
+import { UserDataProvider } from './contexts/UserDataCtx.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <UserDataProvider>
     <App />
+    </UserDataProvider>
   </React.StrictMode>,
 )
