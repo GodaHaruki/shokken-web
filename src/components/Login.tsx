b696d2ab80c401fe205bfa989dcfcce47de866d1
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-05-20 05:58:55 +0000

    change view

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,6 +4,7 @@ import { useUserDataContext } from "../contexts/UserDataCtx"
 export function Login() {
   const email = useRef<string>("")
   const password = useRef<string>("")
+  const f = useUserDataContext();
 
   return (
     <>
@@ -13,7 +14,7 @@ export function Login() {
             Sign in to shokken
           </div>
 
-          <form className="m-3 h-auto rounded-md border border-gray-300 p-3">
+          <form className="m-3 h-auto w-auto rounded-md border border-gray-300 p-3 px-6">
             <div className="my-2">
               <label htmlFor="email" className="mb-2 block">email address</label>
               <input id="email" type="email" onChange={(e) => email.current = e.target.value} className="input-bordered input input-sm w-full max-w-xs" />
@@ -22,13 +23,13 @@ export function Login() {
             <div className="my-3">
               <div className="relative mb-2 w-full">
                 <label htmlFor="password" className="mb-2">password</label>
-                <a href="" className="float-right">forgot a password?</a>
+                <a href="" className="float-right ml-4">forgot a password?</a>
               </div>
               <input id="password" type="password" autoComplete="current-password" className="input-bordered input input-sm w-full max-w-xs" onChange={(e) => password.current = e.target.value} />
 
             </div>
 
-            <button onClick={(_e) => handleOnClick(email.current, password.current)} className="btn-primary btn-sm btn mt-2 w-full text-xs">login</button>
+            <button type="button" onClick={(_e) => {handleOnClick(f,email.current, password.current); console.log(f.getIsLogin())}} className="btn-primary btn-sm btn mt-2 w-full text-xs">login</button>
           </form>
         </div>
       </div>
@@ -36,16 +37,17 @@ export function Login() {
   )
 }
 
-function handleOnClick(email: string, password: string) {
+function handleOnClick(f:{setIsLogin: (b:boolean) => void, setUserName: (s: string) => void}, email: string, password: string) {
   // todo!
   console.log("email: ", email)
   console.log("password: ", password)
 
-  // const { setIsLogin, setUserName } = useUserDataContext()
   // fetch 
   // if success
   // setIsLogin(true)
   // else
+  f.setIsLogin(true)
+  f.setUserName("guest")
 
   // fetch
   // setUserName(name)
