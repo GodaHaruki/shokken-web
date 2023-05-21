9b50cbd5cbfdc9c2ce447bcdc53b35d72269acbc
Okay-Love <haru.11.saki.11@gmail.com>
2023-05-21 17:07:23 +0900

    change props type, add back to login page system

--- src/routing/Auth.tsx
+++ src/routing/Auth.tsx
@@ -1,26 +1,44 @@
 import React, { useState } from "react"
 import { useUserDataContext } from "../contexts/UserDataCtx"
+import { Login } from "../components/Login"
 
 type LoginedProps = {
-  checkingIsLogin: React.ReactNode
   children: React.ReactNode
 }
 
-export function Logined(props: LoginedProps){
-  const [view, setView] = useState(props.checkingIsLogin)
+function Loading() {
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col items-center justify-center">
+        <div className="animate-spin rounded-full border-t-transparent border-[1.5px] border-blue-500 h-5 w-5" />
+        <div>Loading</div>
+      </div>
+    </>
+  )
+}
 
-  const {getIsLogin} = useUserDataContext()
+export function Logined(props: LoginedProps) {
+  const [view, setView] = useState<React.ReactNode>(<Loading />)
+
+  const { getIsLogin } = useUserDataContext()
+
+  // for debug <Loading />
+  // const sleepGetIsLogin = () => new Promise(res => {
+  //   const resolve = res as (value: boolean) => void
+  //   setTimeout(async () => resolve(await getIsLogin()), 3000)
+  // })
+
   getIsLogin().then(v => {
-    if(v){
+    if (v) {
       setView(props.children)
     } else {
-      // todo!: back to login page
+      setView(<Login />)
     }
   })
 
   return (
     <>
-    {view}
+      {view}
     </>
   )
 }
