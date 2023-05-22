b46acfaaae098dd5c618d6180c961dd67da2aaf0
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-05-22 06:47:09 +0000

    impl search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,14 @@ function Header(){
     <header className="sticky flex w-screen flex-wrap items-center justify-between bg-slate-500 py-2 text-white">
       <div className="ml-0 mr-auto">Shokken-web</div>
       <div className="mr-2 flex w-1/2 flex-wrap items-center justify-between">
-        {/* <div className="grid w-2/5 grid-flow-col">
+        <div className="grid w-2/5 grid-flow-col">
           <div>
             <label htmlFor="search">aaa</label>
             <input id="search" type="text" autoComplete="text" />
           </div>
           <div>b</div>
           <div>c</div>
-        </div> */}
+        </div>
         <div className="w-2/5">
         <LoginedOr or={<ul className="flex w-full justify-between">
           <li>1</li>
